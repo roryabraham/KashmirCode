8bea754ec732e5fa1625c43250dfe2955614fc6d
KashmirMonkey <rabraham@sandiego.edu>
2019-04-05 23:43:46 -0700

    updated contact page validation

--- public/Contact/validate.js
+++ public/Contact/validate.js
@@ -1,48 +1,57 @@
 
-let $name = $("#name");
-let $email = $("#email");
+// First wait for the DOM to be fully loaded
+$(function() {
+    let $name = $("#name");
+    let $email = $("#email");
 
-let nameValid = false;
+    let nameValid = false;
 
-let validateName = function (event) {
+    let validateName = function (event) {
 
-    // Validate name input
-    if($name.length < 3 || !$name.value.match('/.+ .+/')) {
-        nameValid = false;
-        $name.addClass("invalid");
-        event.preventDefault();
-    }
-    else {
-        nameValid = true;
-        $name.removeClass("invalid");
-        $name.addClass("valid");
-    }
-};
+        let nameRegex = /.+ .+/;
 
-let validateEmail = function (event) {
+        // Validate name input
+        if($name[0].length < 3 || !nameRegex.test($name[0].value)) {
+            nameValid = false;
+            $name.addClass("invalid");
+            event.preventDefault();
+        }
+        else {
+            nameValid = true;
+            if($name.hasClass("invalid")) {
+                $name.removeClass("invalid");
+            }
+            $name.addClass("valid");
+        }
+    };
 
-    // Email validation done by HTML5 (standard regex used, works for 99.99% of valid email addresses)
-    if($email.checkValidity() === false) {
-        $email.addClass("invalid");
-        event.preventDefault();
-    }
-    else {
-        $email.removeClass("invalid");
-        $email.addClass("valid");
-    }
-};
+    let validateEmail = function (event) {
 
-// Register validation for any input change event
-$name.change(validateName());
-$email.change(validateEmail());
+        // Email validation done by HTML5 (standard regex used, works for 99.99% of valid email addresses)
+        if($email[0].checkValidity() === false) {
+            $email.addClass("invalid");
+            event.preventDefault();
+        }
+        else {
+            if($email.hasClass("invalid")) {
+                $email.removeClass("invalid");
+            }
+            $email.addClass("valid");
+        }
+    };
 
-let validateForm = function (event) {
-    if(!nameValid || !email.checkValidity())
-    {
-        event.preventDefault();
-    }
-    validateEmail(event);
-};
+    // Register validation for any input change event
+    $name.change(validateName);
+    $email.change(validateEmail);
 
-// Register validation for entire form
-$("#submitButton").click(validateForm());
+    let validateForm = function (event) {
+        if(!nameValid || !$email[0].checkValidity())
+        {
+            event.preventDefault();
+        }
+        validateEmail(event);
+    };
+
+    // Register validation for entire form
+    $("#submitButton").click(validateForm);
+});
