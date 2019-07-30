21f7a2ef00f6177a496f2bf5ee2cea7053c078f7
KashmirMonkey <rabraham@sandiego.edu>
2019-07-30 11:47:08 -0700

    updated adventures page and experience/skills sections

--- public/Adventures/adventures.js
+++ public/Adventures/adventures.js
@@ -1,38 +1,94 @@
-let slideIndex = 0;
+let currentSlideIndex = 0;
 let displayState = false;
-const fadeInDuration = 3000;
-const fadeOutDuration = 2000;
+const fadeInDuration = 6000;
+const fadeOutDuration = 3000;
 
-hideSlide = (slides, callback) => {
-    for(let i=0; i < slides.length; i++) {
-        let currSlide = slides.eq(i);
-        if(currSlide.css("display") !== "none") {
-            currSlide.fadeOut(fadeOutDuration, callback);
-        }
-    }
+randomIntInRange = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-showSlide = () => {
+/*displayRandomSlide = (slides) => {
+    let slideIndex = randomIntInRange(1, slides.length);
+    while(slideIndex === currentSlideIndex) {
+        slideIndex = randomIntInRange(1, slides.length);
+    }
+    slides.eq(slideIndex - 1).fadeIn(fadeInDuration);
+    currentSlideIndex = slideIndex;
+};
+
+slideTransition = () => {
     let slides = $(".slide");
 
-    // hide current slide
+    // If there is a currently displayed slide
     if(displayState) {
-        hideSlide(slides, function(){
-            // then display new slide
-            if(++slideIndex > slides.length) {slideIndex = 1}
-            slides.eq(slideIndex - 1).fadeIn(fadeInDuration);
-        });
+        // Fade current slide most of the way out
+        for(let i=0; i < slides.length; i++) {
+            let currSlide = slides.eq(i);
+            if(currSlide.css("display") !== "none") {
+                currSlide.fadeTo(fadeOutDuration, 0.3, () => {
+                    // Then fade in the new slide
+                    displayRandomSlide(slides);
+                    // And fade out the current slide the rest of the way (simultaneously)
+                    currSlide.fadeOut("fast");
+                });
+            }
+        }
     }
     else {
-        // just display new slide
-        if(++slideIndex > slides.length) {slideIndex = 1}
-        slides.eq(slideIndex - 1).fadeIn(fadeInDuration);
+        // Just display the new slide
+        displayRandomSlide(slides);
         displayState = true;
     }
 
-    setTimeout(showSlide, 6500);
+    // repeat every 6.5 seconds
+    setTimeout(slideTransition, 14000);
+};*/
+
+/*
+const slideTransition = () => {
+
+    let slides = $(".slides");
+
+    // find the image on the top of the pile
+    let active = $("#hero_image .active");
+
+    // choose the next image
+    let slideIndex = randomIntInRange(1, slides.length);
+    while(slideIndex === currentSlideIndex) {
+        slideIndex = randomIntInRange(1, slides.length);
+    }
+    let nextSlide = slides.eq(slideIndex);
+    currentSlideIndex = slideIndex;
+
+    // move the next image up in the stack
+    nextSlide.css("z-index",2);
+
+    // Fade out the top image
+    active.fadeOut(fadeOutDuration, () => {
+        // Then move it back to the bottom of the pile, unhide the image, and remove the active class
+        active.css('z-index',1).show().removeClass('active');
+
+        // And move the next image to the top of the pile
+        nextSlide.css('z-index',3).addClass('active');
+    });
 };
+*/
+
+let slideIndex = 1;
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
 
 $(function() {
-    showSlide();
+    // run slide transition every 6 seconds
+    //setInterval(slideTransition, 6000);
+    showSlides()
 });
